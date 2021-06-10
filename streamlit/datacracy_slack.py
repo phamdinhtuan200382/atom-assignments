f61c7ae2e62a27aaf39ae25a6ce4a638e2255a2e
Tuan Pham Dinh <dinhtuan3955@gmail.com>
2021-06-10 20:12:31 +0700

    update

--- streamlit/datacracy_slack.py
+++ streamlit/datacracy_slack.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+<<<<<<< HEAD
 import json
 import requests
 import sys
@@ -193,5 +194,39 @@ if valid_user_id:
     
 else:
     st.markdown('Không tìm thấy Mã Số {}'.format(user_id))
+=======
+import requests # -> Để call API
+import json # -> Xử lý file JSON
+import pandas as pd # -> Thư viện xử lý dữ liệu dạng bảng
+import numpy as np
+import re # -> Thư viện xử lý text: regular expressions
+from datetime import datetime as dt # -> Thư viện xử lý dữ liệu thời 
+
+
+st.title('Tương tác với Cloud Function - UI')
+passcode = st.sidebar.text_input('Passcode', '')
+message = st.sidebar.text_input('Message', '')
+
+#st.text('Token: {}'.format(token))
+    
+endpoint1 = 'https://asia-southeast2-quickstart-313303.cloudfunctions.net/function-1'
+data = {"passcode":passcode, "message":message}
+st.markdown('## Input')
+st.write(data)
+if st.sidebar.button('Call API'):
+    # Ref: https://medium.com/google-cloud/setup-and-invoke-cloud-functions-using-python-e801a8633096
+    response = requests.post(endpoint1, json=data)
+    response_json = json.loads(response.content)
+    st.markdown('## Output')
+    try:
+        st.markdown('**Status**: {}'.format(response_json['status']))
+        st.markdown('**Message**: {}'.format(response_json['message']))
+    except:
+        st.markdown('**Status**: {}'.format('ERROR'))
+    #st.write(response_json)
+    
+    
+
+>>>>>>> parent of 065b2dc (Revert "Update streamlit")
 
 ## Run: streamlit run streamlit/datacracy_slack.py
